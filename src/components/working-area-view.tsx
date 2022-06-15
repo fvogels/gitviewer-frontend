74a729ade544f01356796c92c1a34a7b25137009
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-15 15:48:38 +0200

    reformatted working area view

--- src/components/working-area-view.tsx
+++ src/components/working-area-view.tsx
@@ -3,38 +3,37 @@ import React, { useEffect } from 'react';
 import { FileList, File } from 'components/filelist';
 
 
-export function WorkingAreaView() : JSX.Element
+export function WorkingAreaView(): JSX.Element
 {
-  const [files, setFiles] = React.useState<string[][]>([]);
+    const [files, setFiles] = React.useState<string[][]>([]);
 
-  useEffect(() => {
-    async function fetchData()
-    {
-      const rawData = await fetch('/api/v1/working-area');
-      const data = await rawData.json();
-      setFiles(data['files']);
-    }
+    useEffect(() => {
+        async function fetchData() {
+            const rawData = await fetch('/api/v1/working-area');
+            const data = await rawData.json();
+            setFiles(data['files']);
+        }
 
-    fetchData().catch(console.error);
-  }, []);
-
-  return (
-    <HeaderBox caption='Working Area' captionLocation='west'>
-      <FileList>
-        {files.map(renderFile)}
-      </FileList>
-    </HeaderBox>
-  );
-
-
-  function renderFile(pathParts : string[]) : JSX.Element
-  {
-    const path = pathParts.join('/');
+        fetchData().catch(console.error);
+    }, []);
 
     return (
-      <File key={path}>
-        {path}
-      </File>
+        <HeaderBox caption='Working Area' captionLocation='west'>
+            <FileList>
+                {files.map(renderFile)}
+            </FileList>
+        </HeaderBox>
     );
-  }
+
+
+    function renderFile(pathParts: string[]): JSX.Element
+    {
+        const path = pathParts.join('/');
+
+        return (
+            <File key={path}>
+                {path}
+            </File>
+        );
+    }
 }
