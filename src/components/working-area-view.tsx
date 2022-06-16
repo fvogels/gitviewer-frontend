d1b42c795a8b7b0a96737db4b4ed74fbf9f199e5
Frederic Vogels <frederic.vogels@ucll.be>
2022-06-16 19:29:48 +0200

    selectable working area

--- src/components/working-area-view.tsx
+++ src/components/working-area-view.tsx
@@ -1,11 +1,14 @@
 import { HeaderBox } from 'components/headerbox';
 import React, { useEffect } from 'react';
 import { FileList, File } from 'components/filelist';
+import { SelectionContext } from 'selection-context';
 
 
 export function WorkingAreaView(): JSX.Element
 {
     const [files, setFiles] = React.useState<string[][]>([]);
+    const { selection, setSelection } = React.useContext(SelectionContext);
+
 
     useEffect(() => {
         async function fetchData() {
@@ -29,9 +32,17 @@ export function WorkingAreaView(): JSX.Element
     function renderFile(pathParts: string[]): JSX.Element
     {
         const path = pathParts.join('/');
+        const selectionId = `working-area:${path}`;
+        const isSelected = selection === selectionId;
 
         return (
-            <File key={path} path={path} />
+            <File key={path} path={path} onClick={onClick} isSelected={isSelected} />
         );
+
+
+        function onClick()
+        {
+            setSelection(selectionId);
+        }
     }
 }
